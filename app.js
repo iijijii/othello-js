a1ccfc119419616747394c671f5cd1bc9946a0d7
riri sato <lilysatoo@gmail.com>
2013-06-27 19:04:02 +0900

    石を置ける条件を変更（未完：縦、横のみ動く）

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function draw(){ 
-	var board=new Array(length);
+	var board=new Array();
 	drawSquare();
 	drawFirst();
 	output();
@@ -52,17 +52,17 @@ function newStone(){
 	//ラジオボタンの値を設定
 	var i;
 	if(document.form1.formx.length&&document.form1.formy.length){
-	    for (i = 0; i < document.form1.formx.length; i++) {
+	    for (i = 0; i < document.form1.formx.length; i++) {//0~4
 	        if (document.form1.formx[i].checked) {
-	            var newX=document.form1.formx[i].value;
+	            var newX=parseInt(document.form1.formx[i].value);
 	        }
 	    }
 	    for (i = 0; i < document.form1.formy.length; i++) {
 	        if (document.form1.formy[i].checked) {
-	            var newY=document.form1.formy[i].value;
+	            var newY=parseInt(document.form1.formy[i].value);
 	        }
 	    }
-console.log(board[newX].length);
+
 	    if(board[newX][newY]==cell(newX,newY,"□")){//空いてるかどうか
 			judge(newX,newY,turn);
 		}
@@ -73,49 +73,11 @@ console.log(board[newX].length);
 
 
 function judge(x,y,turn){
-	vertical(x,y,turn);
+//	vertical(x,y,turn);
 	horizon(x,y,turn);
 	if(turn==1){turn=0;}
 	else{turn=1;}
 }
-
-function vertical(x,y,turn){//
-
-	var party="●";
-	var enemy="○";
-	if(turn==1){
-		party="○";
-		enemy="●";
-	}
-
-	var i=1;
-	if(x<length/2){	
-		while(board[x+i][y]==enemy){
-			i++;
-		}
-		if(i>=2&&board[x+i][y]==party){
-			for(var j=x;j<x+i;j++){			
-				board[j][y]=cell(j,y,party);
-			}
-		}
-		else{
-			checker=false;
-		}
-	}if(x>=length/2){
-		while(board[x-i][y]==enemy){
-			i++;
-		}
-		if(i>=2&&board[x-i][y]==party){
-			for(var j=x;j>x-i;j--){			
-				board[j][y]=cell(j,y,party);
-			}
-		}
-		else{
-			checker= false;
-		}
-	}	
-}
-
 
 function horizon(x,y,turn){//
 
@@ -127,31 +89,76 @@ function horizon(x,y,turn){//
 	}
 
 	var i=1;
-	if(y<length/2){	
-		while(board[x][y+i]==enemy){
-			i++;
+	
+	//(board[x+i][y]!=".")
+
+	while(board[x+i][y]==enemy){
+		i++;
+	}
+	if(i>=2&&board[x+i][y]==party){
+		for(var j=x;j<x+i;j++){			
+			board[j][y]=cell(j,y,party);
 		}
-		if(i>=2&&board[x][y+i]==party){
-			for(var j=y;j<y+i;j++){			
-				board[x][j]=cell(x,j,party);
-			}
+	}
+	
+
+	//else{
+	
+	//}
+
+	var k=1;
+	while(board[x-k][y]==enemy){
+		k++;
+	}
+	if(k>=2&&board[x-k][y]==party){
+		for(var j=x;j>x-k;j--){			
+			board[j][y]=cell(j,y,party);
 		}
-		else{
-			
+	}
+	//else{
+	
+	//}
+}	
+
+
+
+
+function verical(x,y,turn){//
+
+	var party="●";
+	var enemy="○";
+	if(turn==1){
+		party="○";
+		enemy="●";
+	}
+
+	var i=1;
+	
+	while(board[x][y+i]==enemy){
+		i++;
+	}
+	if(i>=2&&board[x][y+i]==party){
+		for(var j=y;j<y+i;j++){			
+			board[x][j]=party;
 		}
-	}if(x>=length/2){
-		while(board[x][y-i]==enemy){
-			i++;
+	}
+	//else{
+		
+	//}
+
+
+	while(board[x][y-i]==enemy){
+		i++;
+	}
+	if(i>=2&&board[x][y-i]==party){
+		for(var j=y;j>y-i;j--){			
+			board[x][j]=party;
 		}
-		if(i>=2&&board[x][y-i]==party){
-			for(var j=y;j>y-i;j--){			
-				board[x][j]=cell(x,j,party);
-			}
-		}
-		else{
-			
-		}
-	}	
+	}
+	//else{
+		
+	//}
+		
 }
 
 function diagonal(){
