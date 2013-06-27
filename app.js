9a6322f8f24c19b58e6f8e7d5959947117966d64
riri sato <lilysatoo@gmail.com>
2013-06-27 12:08:33 +0900

    石を置ける条件を追加（未完成）
    黒を置く場合の条件

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ function cell(x,y,moji){
 	return '<span id="id'+x+y+'">'+moji+'</span>';
 }
 
-
-
 function drawSquare(){
 	for(var x=0;x<4;x++){
 		board[x]=new Array(4);
@@ -40,13 +38,12 @@ function output(){
 var turn=0;//●のターン,turn=1は○のターン
 
 function newStone(){
-
+	//ラジオボタンの値を設定
 	var i;
 	if(document.form1.formx.length&&document.form1.formy.length){
 	    for (i = 0; i < document.form1.formx.length; i++) {
 	        if (document.form1.formx[i].checked) {
 	            var newX=document.form1.formx[i].value;
-
 	        }
 	    }
 	    for (i = 0; i < document.form1.formy.length; i++) {
@@ -54,15 +51,68 @@ function newStone(){
 	            var newY=document.form1.formy[i].value;
 	        }
 	    }
-	    if(board[newX][newY]==cell(newX,newY,"□")){
+
+	    if(board[newX][newY]==cell(newX,newY,"□")){//空いてるかどうか
 		    if(turn==0){
-		    	board[newX][newY]=cell(newX,newY,"○");
-		    	turn=1;
+		    	if(judge(newY,newY)==true){
+			    	board[newX][newY]=cell(newX,newY,"●");
+			    	turn=1;
+			    }
 			}else{
-				board[newX][newY]=cell(newX,newY,"●");
-		    	turn=0;
+				//if(judge()==true){
+					board[newX][newY]=cell(newX,newY,"○");
+			    	turn=0;
+			    //}
 			}
 		}
+		console.log(turn);
 	    output();
-	}   
+	} 
+}
+
+
+
+function judge(x,y){
+	vertical(y);
+	horizon(x);
+}
+
+function vertical(y){//
+	var blacks=new Array();//黒があるところのインデックスを格納
+	var whites=new Array();
+
+	for(var i=0;i<4;i++){
+		var isBlackOne=$.inArray(cell(i,y,"●"),board[y]);//黒があるところのインデックス
+		var isWhiteOne=$.inArray(cell(i,y,"○"),board[y]);
+		console.log("i="+i+isBlackOne+isWhiteOne);
+		if(isBlackOne!=-1){
+			blacks.push(isBlackOne);
+		}
+		if(isWhiteOne!=-1){
+			whites.push(isWhiteOne);
+		}	
+	}
+	 if(whites.length=1){
+	 	return true;
+	 }
+}
+
+function horizon(x){
+	var blacks=new Array();
+	var whites=new Array();
+
+	for(var i=0;i<4;i++){		
+		var isBlackOne=$.inArray(cell(x,i,"●"),board[x]);//黒があるところのインデックス
+		var isWhiteOne=$.inArray(cell(x,i,"○"),board[x]);
+		console.log("i="+i+isBlackOne+isWhiteOne);
+		if(isBlackOne!=-1){
+			blacks.push(isBlackOne);
+		}
+		if(isWhiteOne!=-1){
+			whites.push(isWhiteOne);
+		}	
+	}
+	if(whites.length=1){
+		return true;
+	}
 }
